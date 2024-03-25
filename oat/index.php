9289645721c18d07a77584f17c7a89a6dfe37f0e
root <oatt16@gmail.com>
2024-03-25 22:02:31 +0700

    logo and form styling

--- oat/index.php
+++ oat/index.php
@@ -1,15 +1,16 @@
 <?php include 'filesLogic.php';
 // Add Prefix
 function genRandomString() {
-    $length = 10;
+    $length = 7;
     $characters = "0123456789abcdefghijklmnopqrstuvwxyz";
     $string = "";
+    $time = time();
 
     for ($p = 0; $p < $length; $p++) {
         $string .= $characters[mt_rand(0, strlen($characters)-1)];
     }
 
-    return $string;
+    return "[" . date("h:i:s",$time) . "] - " . $string;
 }
 
 ?>
@@ -22,17 +23,19 @@ function genRandomString() {
   <body>
     <div class="container">
       <div class="row">
+        <div id="form">
         <form action="index.php" method="post" enctype="multipart/form-data" >
           <h3>Upload File</h3>
           <input type="file" name="myfile"> <br>
           <button type="submit" name="save">Upload</button>
           <input type="hidden" name="myfile_newname" value="<?php print genRandomString(); ?>.jpg" />
         </form>
-        <div style="display:flex; justify-content: center; align-items: center;" class="row">
-            <!-- Button to lead to download page -->
-            <button style="margin-top:10px;" onclick="window.location.href='/oat/downloads.php';">Download</button>
+          <button onclick="window.location.href='/oat/downloads.php';">Download</button>
         </div>
       </div>
+    </div>
+    <div id="logo"> 
+        <img id="logo" src="./assets/pakjung.png" > 
     </div>
   </body>
 </html>
